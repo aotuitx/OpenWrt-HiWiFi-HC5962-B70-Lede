0f82e6f8f820c9a775d943e9c9733a54dbb69a67
aotuitx <45528676+aotuitx@users.noreply.github.com>
2022-04-24 01:01:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 
 
 #set competence
-chmod -R 755 files
+#chmod -R 755 files
 
 #echo '修改文件'
 #rm -rf package/base-files/files/etc/banner
