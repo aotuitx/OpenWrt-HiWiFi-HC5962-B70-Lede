adff2f243fc36f7962e682b086eaa4a763562881
aotuitx <45528676+aotuitx@users.noreply.github.com>
2022-04-23 22:25:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
+
+
+#set competence
+#chmod -R 755 files
