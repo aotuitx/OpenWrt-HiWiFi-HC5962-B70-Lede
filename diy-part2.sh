726982c27e3751bb95b4a50f8519922f14e97f99
aotuitx <45528676+aotuitx@users.noreply.github.com>
2022-04-24 09:21:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,8 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
+#禁用WAN6生成
+sed -i 's/network_find_wan6/#network_find_wan6/g' package/base-files/files/lib/functions/network.sh
 
 
 #设置文件权限
