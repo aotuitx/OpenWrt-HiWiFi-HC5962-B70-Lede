8cfb73251d724c2e9ebedcf3a5d9d2f7043b540a
aotuitx <45528676+aotuitx@users.noreply.github.com>
2022-04-25 20:55:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,11 +17,11 @@ sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 #禁用IPv6 DHCP，包含单引号的sed外部直接用双引号
 sed -i "s/ipv6='1'/ipv6='0'/g" package/base-files/files/bin/config_generate
 # 禁止解析 IPv6 DNS 记录
-sed -i '/dnsmasq/a\ \t\\toption filter_aaaa '1'' package/network/services/dnsmasq/files/dhcp.conf
+sed  '/dnsmasq/a\\\\toption filter_aaaa\t\\t1'  package/network/services/dnsmasq/files/dhcp.conf
 #不记录日志
-sed -i '/dnsmasq/a\ \t\\toption quietdhcp '1'' package/network/services/dnsmasq/files/dhcp.conf
+sed -i '/dnsmasq/a\\\\toption quietdhcp\t\\t1' package/network/services/dnsmasq/files/dhcp.conf
 #DHCP顺序分配 IP /etc/config/dhcp 中 config dnsmasq 字段下。
-sed -i '/dnsmasq/a\ \t\\toption sequential_ip '1'' package/network/services/dnsmasq/files/dhcp.conf
+sed -i '/dnsmasq/a\\\\toption sequential_ip\t\\t1' package/network/services/dnsmasq/files/dhcp.conf
 # 禁用内置的 IPv6 管理， /etc/config/network 中 config interface 'wan'、config interface 'lan' 字段下
 # option delegate '0'
 
